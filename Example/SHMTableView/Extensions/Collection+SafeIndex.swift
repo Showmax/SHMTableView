7e20a851f6dfe0068b8d14bb05a46e3697af964a
Ondrej Macoszek <ondrej.macoszek@showmax.com>
2017-07-19 15:55:56 +0200

    T19508 Removed redundant new lines.

--- Example/SHMTableView/Extensions/Collection+SafeIndex.swift
+++ Example/SHMTableView/Extensions/Collection+SafeIndex.swift
@@ -10,9 +10,9 @@ import Foundation
 
 public extension Collection where Indices.Iterator.Element == Index 
 {
-    
     /// Returns the element at the specified index iff it is within bounds, otherwise nil.
-    subscript (safe index: Index) -> Generator.Element? {
+    subscript (safe index: Index) -> Generator.Element?
+    {
         return indices.contains(index) ? self[index] : nil
     }
 }
